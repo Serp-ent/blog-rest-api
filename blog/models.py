213fffffd4815564876e613b152a5b94560a8fb4
Serp-ent <kacperurbanski002@gmail.com>
2025-02-20 15:10:30 +0100

    Create author automatically on user creation

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author')
     bio = models.TextField()
 
     def __str__(self) -> str:
         return f"{self.name} ({self.id})"
+
+@receiver(post_save, sender=User)
+def create_author(sender, instance, created, **kwargs):
+    if created:
+        Author.objects.create(user=instance)
 
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateField(auto_now_add=True)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='blogs')
 
     def __str__(self) -> str:
         return f"{self.title} ({self.id})"
--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -1,2 +1,31 @@
-def test_pytest_is_working():
-    assert 1 == 1
+import pytest
+from django.contrib.auth.models import User
+from blog.models import Blog, Author
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "username,is_admin",
+    [
+        ("testuser", False),
+        ("ash", False),
+        ("admin", True),
+        ("admin2", True),
+    ],
+    ids=["casual User 1", "casual User 2", "superuser 1", "superuser 2"],
+)
+def test_author_model_created_on_user_creation(username, is_admin):
+    # Act
+    if is_admin:
+        create_user = User.objects.create_superuser
+    else:
+        create_user = User.objects.create_user
+
+    user = create_user(username=username)
+
+    # Assert
+    assert (
+        getattr(user, "author", None) is not None
+    ), f"Author model was not created on {'Superuser' if is_admin else 'Casual User'} creation"
+    author = Author.objects.filter(user=user).get()
+    assert author.user == user
