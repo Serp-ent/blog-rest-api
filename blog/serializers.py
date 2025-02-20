c1faa7f9df861fd4a9d478745467cb518651fce5
Serp-ent <kacperurbanski002@gmail.com>
2025-02-20 12:50:43 +0100

    Add hyperlinks between models

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,19 +2,40 @@ from rest_framework import serializers
 from blog.models import Author, Blog, Comment
 
 
-class AuthorSerializer(serializers.ModelSerializer):
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+
+    def to_representation(self, instance):
+        from .serializers import CommentSerializer
+
+        request = self.context.get("request")
+
+        data = super().to_representation(instance)
+        comments = instance.comments.order_by("-created_at")[:5]
+        data["comments"] = CommentSerializer(
+            comments, many=True, context={"request": request}
+        ).data
+
+        return data
+
     class Meta:
         model = Author
-        fields = ["name", "email", "bio"]
+        fields = ["url", "name", "email", "bio"]
 
 
-class BlogSerializer(serializers.ModelSerializer):
+class BlogSerializer(serializers.HyperlinkedModelSerializer):
     def to_representation(self, instance):
+        request = self.context.get("request")
+
         data = super().to_representation(instance)
-        data["author"] = AuthorSerializer(instance.author).data
-        
-        comments = Comment.objects.order_by('-created_at')[:5]
-        data['comments'] = CommentSerializer(comments, many=True).data
+
+        data["author"] = AuthorSerializer(
+            instance.author, context={"request": request}
+        ).data
+
+        comments = instance.comments.order_by("-created_at")[:5]
+        data["comments"] = CommentSerializer(
+            comments, many=True, context={"request": request}
+        ).data
         return data
 
     class Meta:
@@ -28,10 +49,11 @@ class BlogSerializer(serializers.ModelSerializer):
         ]
 
 
-class CommentSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Comment
         fields = [
+            "url",
             "blog",
             "author",
             "created_at",
