6d2104c2cb330c1374196e7f25553c3dfdccad70
Serp-ent <kacperurbanski002@gmail.com>
2025-02-21 12:04:57 +0100

    Add throtling for anon users

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -35,6 +35,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+   'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+    }
 }
 
 # Application definition
