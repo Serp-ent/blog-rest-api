7f774f3d877c74cff8599cd341f2829b9ff53254
Serp-ent <kacperurbanski002@gmail.com>
2025-02-22 21:14:29 +0100

    Add comments viewing under post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.request import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from blog.permissions import IsBlogOwner
 from rest_framework import status
@@ -8,7 +9,12 @@ from rest_framework.decorators import action
 from rest_framework import generics
 from blog.models import Author, Blog, Comment
 from rest_framework.views import APIView
-from blog.serializers import AuthorSerializer, BlogSerializer, CommentSerializer, UserRegistrationSerializer
+from blog.serializers import (
+    AuthorSerializer,
+    BlogSerializer,
+    CommentSerializer,
+    UserRegistrationSerializer,
+)
 from rest_framework import permissions
 
 
@@ -19,7 +25,6 @@ class AuthorViewset(viewsets.ReadOnlyModelViewSet):
 
 
 # TODO: add filtering by title/author
-# TODO: implement nested router for blog comments
 # TODO: generate OpenAPI/Swagger documentation
 # TODO: implement versioned API (v1/v2)
 class BlogViewset(viewsets.ModelViewSet):
@@ -31,7 +36,11 @@ class BlogViewset(viewsets.ModelViewSet):
         user = self.request.user
         return serializer.save(author=user.author)
 
-    @action(detail=True, methods=['POST'], permission_classes=[permissions.IsAuthenticated, IsBlogOwner])
+    @action(
+        detail=True,
+        methods=["POST"],
+        permission_classes=[permissions.IsAuthenticated, IsBlogOwner],
+    )
     def publish(self, request, pk=None):
         blog: Blog = self.get_object()
         blog.is_published = True
@@ -40,7 +49,15 @@ class BlogViewset(viewsets.ModelViewSet):
         serializer = self.get_serializer(blog)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @action(detail=True, methods=["GET"])
+    def comments(self, request: Request, pk=None):
+        blog_post = self.get_object()
+        comments = Comment.objects.filter(blog=blog_post)
+        serializer = CommentSerializer(
+            comments, many=True, context={"request": request}
+        )
 
+        return Response(serializer.data)
 
 
 class CommentViewset(viewsets.ModelViewSet):
@@ -58,8 +75,9 @@ class AuthTestView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        content = {'message': "Hello! You're authenticated using JWT"}
+        content = {"message": "Hello! You're authenticated using JWT"}
         return Response(content)
+
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
